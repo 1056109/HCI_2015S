cffaba14154415d942b334249bde5d5121a76942
Vorschi <e1026777@student.tuwien.ac.at>
2015-05-11 16:05:55 +0200

    beginning of work
    zoom animation, custom button icon, show position, draw buttons over map

--- HCISS2015/app/src/main/java/at/at/tuwien/hci/hciss2015/MainActivity.java
+++ HCISS2015/app/src/main/java/at/at/tuwien/hci/hciss2015/MainActivity.java
@@ -1,19 +1,35 @@
 package at.at.tuwien.hci.hciss2015;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.android.gms.maps.model.TileProvider;
 import com.google.android.gms.maps.model.UrlTileProvider;
 
+import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * sources @
@@ -23,9 +39,8 @@ import java.net.URL;
 
 public class MainActivity extends FragmentActivity {
 
-    private GoogleMap map;
+    private GoogleMap mMap;
 
-    /*
     private final LatLng VIENNA = new LatLng(48.209272, 16.372801);
 
     private static final int MIN_ZOOM = 12;
@@ -33,27 +48,93 @@ public class MainActivity extends FragmentActivity {
     private static final int MAX_ZOOM = 16;
 
     private static final String TILE_SERVER_URL = "http://tile.openstreetmap.org/";
-    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        map = ((SupportMapFragment) getSupportFragmentManager()
+        mMap = ((SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map)).getMap();
 
-        //map.setMapType(GoogleMap.MAP_TYPE_NONE);
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(VIENNA, 12));
 
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(VIENNA)         // Sets the center of the map to Mountain View
+                .zoom(12)               // Sets the zoom
+                .build();               // Creates a CameraPosition from the builder
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, new GoogleMap.CancelableCallback() {
+
+            @Override
+            public void onFinish() {        //When animation is finished, do some stuff like drawing icons and showing position
+                mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(48.208587, 16.372301))
+                                .title("Testmarker 1")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.hospital))
+                );
+
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);      //show MyLocation-Button
+                mMap.setMyLocationEnabled(true);        //Show my location
+            }
+
+            @Override
+            public void onCancel() {       //if animation gets cancelled do something
+
+            }
+        });
+    }
+}
+
+        //ArrayList<LatLng> hospitals = new ArrayList<>();
+
+
+        /**
+         * Location Listener, maybee needed or not, so the code is provided here to be sure
+         */
+        /*
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+// Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                LatLng pos = new LatLng(location.getLatitude(), location.getLongitude());
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 12));
+                mMap.addMarker(new MarkerOptions()
+                                .position(pos)
+                                .title("Hospital 1")
+                        // .icon(BitmapDescriptorFactory.fromFile("res/hospital.bmp"))
+                );-
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+// Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        */
+
+
+        /**
+         * Tile Overlay von Amer
+         */
+        /*
         TileOverlayOptions opts = new TileOverlayOptions();
 
         opts.tileProvider(new MapBoxOnlineTileProvider("mapbox.light"));
 
         opts.zIndex(5);
 
-        TileOverlay overlay = map.addTileOverlay(opts);
+        TileOverlay overlay = mMap.addTileOverlay(opts);
 
-        /*
         mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
         TileProvider tileProvider = new UrlTileProvider(256, 256) {
             @Override
@@ -84,16 +165,47 @@ public class MainActivity extends FragmentActivity {
         System.out.println("is overlay visible: " + tileOverlay.isVisible());
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(VIENNA, 15));
-        */
+
     }
 
-    /*
+
     private boolean checkTileExists(int x, int y, int zoom) {
         if ((zoom < MIN_ZOOM || zoom > MAX_ZOOM)) {
             return false;
         }
         return true;
     }
-    */
-
 }
+
+   /*protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }*/
+
+
+
+
