3f2b7d09880a41e94c2908c7f03bc4fb87be19c2
Vorschi <e1026777@student.tuwien.ac.at>
2015-06-09 15:01:31 +0200

    custom gps dialog

--- HCISS2015/app/src/main/java/at/at/tuwien/hci/hciss2015/InitActivity.java
+++ HCISS2015/app/src/main/java/at/at/tuwien/hci/hciss2015/InitActivity.java
@@ -1,16 +1,19 @@
 package at.at.tuwien.hci.hciss2015;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 
 import at.at.tuwien.hci.hciss2015.util.SharedPreferencesHandler;
 
@@ -23,6 +26,7 @@ public class InitActivity extends Activity {
 
     private Handler myHandler;
     private Runnable runnable;
+    private Dialog dialog;
 
     private SharedPreferencesHandler sharedPrefs;
 
@@ -38,41 +42,32 @@ public class InitActivity extends Activity {
 
         /*
          * message for checking gps-service
-         * turned off for testing
-        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+         * turned off for testing*/
+        /*if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
         } else {
             start();
-        }
-        */
-
+        }*/
         start();
     }
 
+
+
     private void buildAlertMessageNoGps() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View layout = li.inflate(R.layout.gpsdialog, null);
+        dialog = new Dialog(this);
 
-        builder.setMessage(getResources().getString(R.string.gps_message))
-                .setCancelable(false)
-                .setView(layout)
-                .setPositiveButton("Ja, einschalten", new DialogInterface.OnClickListener() {
-                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                        start();
-                    }
-                })
-                .setNegativeButton("Nein, App beenden", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                        dialog.cancel();
-                        finish();
-                    }
-                });
-        final AlertDialog alert = builder.create();
-        alert.show();
+        LayoutInflater li = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = li.inflate(R.layout.gpsdialog, null, false);
+
+        dialog.setContentView(layout);
+        dialog.setCancelable(false);
+        Window window = dialog.getWindow();
+        window.setBackgroundDrawableResource(android.R.color.transparent);
+
+        dialog.show();
     }
+
 
     private void start() {
         runnable = new Runnable() {
@@ -96,6 +91,17 @@ public class InitActivity extends Activity {
         };
         myHandler.postDelayed(runnable, 2000);
     }
+
+    public void gpsOn(View view) {
+        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+        start();
+    }
+
+    public void destroy(View view){
+        dialog.cancel();
+        System.exit(1);
+    }
+
 
     @Override
     protected void onDestroy() {
