e5e3023d4c12344745acb3ac2ad2f70ffe08e86f
horstlb <alexbrandner@gmx.net>
2015-07-13 18:38:37 +0200

    app-release with correct db

--- HCISS2015/app/src/main/java/at/at/tuwien/hci/hciss2015/persistence/MyDatabaseHelper.java
+++ HCISS2015/app/src/main/java/at/at/tuwien/hci/hciss2015/persistence/MyDatabaseHelper.java
@@ -9,7 +9,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
  */
 public class MyDatabaseHelper extends SQLiteAssetHelper {
 
-    private static final String DATABASE_NAME = "hci-alex.db";
+    private static final String DATABASE_NAME = "hci.db";
     private static final int DATABASE_VERSION = 1;
 
     public MyDatabaseHelper(Context context) {
